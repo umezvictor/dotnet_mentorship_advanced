0b9019d894c7cfff4773af21428ee3282792c669
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2025-11-04 20:28:12 +0200

    removed mediatr package

--- src/CartServices/BLL/DependencyInjection.cs
+++ src/CartServices/BLL/DependencyInjection.cs
@@ -14,7 +14,6 @@ public static class DependencyInjection
     public static IServiceCollection AddPackages(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddScoped<ICartService, CartService>();
         return services;
     }
