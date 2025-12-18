c77d8cc971da4d6f90a5330524208a1ce3aa66d9
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2025-12-18 13:53:04 +0200

    stylefix

--- src/CartServices/DAL/Entities/CartItem.cs
+++ src/CartServices/DAL/Entities/CartItem.cs
@@ -1,9 +1,9 @@
 ﻿namespace DAL.Entities;
 public class CartItem
 {
-    public int Id { get; set; } //refers to Product Id
-    public required string Name { get; set; }
-    public string Image { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
+	public int Id { get; set; } //refers to Product Id
+	public required string Name { get; set; }
+	public string Image { get; set; } = string.Empty;
+	public decimal Price { get; set; }
+	public int Quantity { get; set; }
 }
