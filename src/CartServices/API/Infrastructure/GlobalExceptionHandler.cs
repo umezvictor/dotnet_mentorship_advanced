026d83f061a97ba703c6e4af4eabe2dae5c151d1
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2025-12-18 13:37:51 +0200

    stylefix

--- src/CartServices/API/Infrastructure/GlobalExceptionHandler.cs
+++ src/CartServices/API/Infrastructure/GlobalExceptionHandler.cs
@@ -6,29 +6,29 @@ namespace API.Infrastructure;
 internal sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IProblemDetailsService problemDetailsService)
    : IExceptionHandler
 {
-    public async ValueTask<bool> TryHandleAsync(
-    HttpContext httpContext,
-    Exception exception,
-    CancellationToken cancellationToken)
-    {
-        logger.LogError(exception, "An exception occurred");
-        var problemDetails = new ProblemDetails
-        {
-            Status = exception switch
-            {
-                ArgumentException => StatusCodes.Status400BadRequest,
-                _ => StatusCodes.Status500InternalServerError
-            },
-            Title = "An error occurred",
-            Type = exception.GetType().Name,
-            Detail = exception.Message
-        };
+	public async ValueTask<bool> TryHandleAsync(
+	HttpContext httpContext,
+	Exception exception,
+	CancellationToken cancellationToken)
+	{
+		logger.LogError(exception, "An exception occurred");
+		var problemDetails = new ProblemDetails
+		{
+			Status = exception switch
+			{
+				ArgumentException => StatusCodes.Status400BadRequest,
+				_ => StatusCodes.Status500InternalServerError
+			},
+			Title = "An error occurred",
+			Type = exception.GetType().Name,
+			Detail = exception.Message
+		};
 
-        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
-        {
-            Exception = exception,
-            HttpContext = httpContext,
-            ProblemDetails = problemDetails
-        });
-    }
+		return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+		{
+			Exception = exception,
+			HttpContext = httpContext,
+			ProblemDetails = problemDetails
+		});
+	}
 }
