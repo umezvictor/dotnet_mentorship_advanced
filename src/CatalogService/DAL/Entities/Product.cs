33f209e878ab99ba8445c60b06204560b05ddebd
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2025-12-18 13:18:45 +0200

    stylefix

--- src/CatalogService/DAL/Entities/Product.cs
+++ src/CatalogService/DAL/Entities/Product.cs
@@ -4,15 +4,15 @@ namespace DAL.Entities;
 public sealed class Product : AuditableEntity
 {
 
-    [Key]
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public string Description { get; set; } = string.Empty;
-    [Required]
-    public decimal Price { get; set; }
-    [Required]
-    public int Amount { get; set; }
-    public string Image { get; set; } = string.Empty;
-    public int CategoryId { get; set; }
-    public required Category Category { get; set; }
+	[Key]
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public string Description { get; set; } = string.Empty;
+	[Required]
+	public decimal Price { get; set; }
+	[Required]
+	public int Amount { get; set; }
+	public string Image { get; set; } = string.Empty;
+	public int CategoryId { get; set; }
+	public required Category Category { get; set; }
 }
